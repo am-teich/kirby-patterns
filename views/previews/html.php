be2de077204fefd8648c0b0150ee7722b78b0db4
crealistiques <info@crealistiques.de>
2019-04-10 16:09:34 +0200

    Fix some path in order to fix plugin use with kirby running in subdirectory.

--- views/previews/html.php
+++ views/previews/html.php
@@ -1,11 +1,26 @@
-<? use \Kirby\Toolkit\Html; ?>
+<?
+    use \Kirby\Toolkit\Html;
+    use Kirby\Patterns\Lab;
+
+    $baseUrl = $pattern->url() . '/' . $file->filename();
+?>
 <div class="toolbar bar">
   <nav class="buttons">
-    <?= Html::a('?view=php', 'PHP', ['class' => r($view == 'php', 'active')]) ?>
-    <?= Html::a('?view=html', 'HTML', ['class' => r($view == 'html', 'active')]) ?>
-    <?= Html::a('?view=htmlpreview', 'HTML Preview', ['class' => r($view == 'htmlpreview', 'active')]) ?>
-    <?= Html::a('?view=preview', 'Preview', ['class' => r($view == 'preview', 'active')]) ?>
-    <?= Html::a($pattern->url() . '/preview', 'Raw', ['target' => '_blank']) ?>
+    <?= Html::a($baseUrl . '?view=php', 'PHP', ['class' => r($view == 'php', 'active')]) ?>
+    <?= Html::a($baseUrl . '?view=html', 'HTML', ['class' => r($view == 'html', 'active')]) ?>
+    <?
+        if (Lab::$mode !== 'test') {
+            echo Html::a($baseUrl . '?view=htmlpreview', 'HTML Preview', ['class' => r($view == 'htmlpreview', 'active')]);
+        }
+    ?>
+    <?= Html::a($baseUrl . '?view=preview', 'Preview', ['class' => r($view == 'preview', 'active')]) ?>
+    <?
+        if (Lab::$mode === 'test') {
+            echo Html::a($pattern->url() . '/test', 'Raw', ['target' => '_blank']);
+        } else {
+            echo Html::a($pattern->url() . '/preview', 'Raw', ['target' => '_blank']);
+        }
+    ?>
   </nav>
 </div>
 
